ac3785c9b4f2599a8f2500a3d2028bfdede7485c
RJC <49545972+rjcastaneda@users.noreply.github.com>
2020-02-12 21:49:58 -0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,4 +1,11 @@
-
+/*
+ * RJCastaneda
+ * File Name: graph.h
+ *
+ * Programmer Notes:
+ * header file that contains the prototyped graph class.
+ *
+ */
 
 #ifndef CS310_LAB_3_1_GRAPH_H
 #define CS310_LAB_3_1_GRAPH_H
